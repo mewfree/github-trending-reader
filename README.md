276b557647c602d9a149f267b833014e4e645f61
mewfree <damien.gonot@gmail.com>
2016-01-03 00:01:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# github-trending-reader
+Lists all the GitHub's new trending repos
---
+++ main.rb
@@ -0,0 +1,37 @@
+require 'nokogiri'
+require 'net/http'
+require 'redis'
+require 'sinatra'
+require 'slim'
+
+# Initiating Redis
+redis = Redis.new
+
+# GitHub trending page's URI
+uri = URI('https://github.com/trending')
+
+get '/' do
+  html = Net::HTTP.get(uri)
+  page = Nokogiri::HTML(html)
+  base = page.xpath('//ol/*/h3/a/@href')
+
+  repos = []
+
+  base.each do |repo|
+    repo = repo.to_s
+    split = repo.split('/')
+
+    author = split[1]
+    name = split[2]
+    key = author+'/'+name
+    link = 'https://github.com/'+key
+
+    if (!redis.get(key))
+      repos.push({ :key => key, :link => link })
+      redis.setex(key, 30*24*60*60, '')
+    end
+  end
+
+  @repos = repos
+  slim :index
+end
